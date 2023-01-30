159243f326dc12b39e81560d51b097d51a4ccbe0
Lycia Stenson <66551408+LennonMcCartney@users.noreply.github.com>
2023-01-30 07:05:41 +0000

    Line drawn relative to player, shape detection in progress

--- Assets/Scripts/PinchTest.cs
+++ Assets/Scripts/PinchTest.cs
@@ -1,14 +1,21 @@
+using Leap.Unity;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class PinchTest : MonoBehaviour {
+	
+	[SerializeField] private GameObject playerCapsule;
+
+	[SerializeField] private PinchDetector pinchDetector;
+
+	[SerializeField] private GameObject drawBackground;
+
+	[SerializeField] private List<Vector2> targetPositions;
+	[SerializeField] private Vector2 targetSize;
 
 	public bool pinching = false;
-
-	//[SerializeField] private List<Vector2> drawPoints;
 	[SerializeField] private List<Vector3> drawPoints;
 
 	LineRenderer lineRenderer;
@@ -22,19 +29,20 @@ public class PinchTest : MonoBehaviour {
 
 	public void FixedUpdate() {
 		if ( pinching ) {
+			drawBackground.SetActive( true );
 			counter += Time.deltaTime;
 			if ( counter > 0.01f ) {
-				drawPoints.Add( transform.localPosition );
-				//drawPoints.Add( new Vector2( transform.localPosition.x, transform.localPosition.y ) );
-				//Debug.Log( new Vector2( transform.localPosition.x, transform.localPosition.y ) );
+				Vector2 newPoint = new Vector2( pinchDetector.transform.localPosition.x, pinchDetector.transform.localPosition.y );
+				drawPoints.Add( new Vector3( newPoint.x, newPoint.y, 2 ) );
 				counter = 0.0f;
 			}
-			//Debug.Log( "Pinch at " + new Vector2( transform.localPosition.x, transform.localPosition.y ) );
+		} else {
+			drawBackground.SetActive( false );
 		}
 
 		lineRenderer.positionCount = drawPoints.Count;
 		for ( int i = 0; i < drawPoints.Count; i++ ) {
-			lineRenderer.SetPosition(i, drawPoints[i] );
+			lineRenderer.SetPosition( i, drawPoints[i] );
 		}
 	}
 
